53744c2fec4ac0a01cc80333de978c8f711a7d33
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2024-05-12 11:15:16 +0300

    a

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-    },
-    {
-      path: "/about",
-      name: "about",
-    },
-  ],
-});
+})
 
-export default router;
+export default router
